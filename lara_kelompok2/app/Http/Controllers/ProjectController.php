f07f4d3f5d0e875affbd90ab685b7e3427fe5f51
Hanif Ihsanul Huda <hanif.ihsanul@sonar.id>
2026-09-09 20:24:18 +0700

    Fitur 2

---
+++ lara_kelompok2/app/Http/Controllers/ProjectController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Project;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class ProjectController extends Controller
+{
+    public function index()
+    {
+        $projects = Project::where('user_id', auth()->id())
+            ->latest()
+            ->get();
+
+        return Inertia::render('Projects/Index', [
+            'projects' => $projects,
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'description' => 'nullable|string',
+        ]);
+
+        Project::create([
+            'user_id' => auth()->id(),
+            'name' => $request->name,
+            'description' => $request->description,
+        ]);
+
+        return redirect()->back();
+    }
+}
